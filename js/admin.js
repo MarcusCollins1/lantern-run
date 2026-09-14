af9ea22b11483830f2c1a1ce40f6495a71541769
Marcus Collins <mjdj27123@gmail.com>
2026-09-14 16:55:11 +0100

    Remove unused import of setSaveHandler from game.js

--- js/admin.js
+++ js/admin.js
@@ -7,7 +7,6 @@ import {
 import { auth } from "./firebase.js";
 
 import { levels } from "./levels.js";
-import { setSaveHandler } from "./game.js";
 
 // ==================================================
 // DOM
